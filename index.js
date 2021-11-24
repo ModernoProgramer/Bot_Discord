9a3ea7a2a777d766bf69c65db77ba697ea5184b7
Moderno <83356984+ModernoProgramer@users.noreply.github.com>
2021-11-24 12:32:39 -0300

    Update index.js

--- index.js
+++ index.js
@@ -16,15 +16,15 @@ client.on('message', message => {
     const args = message.content.slice(prefix.length).trim().split(/ +/);
     const command = args.shift();
 
-    if (command === "ping") {
+    if (command === "ping"|| command ==="Ping") {
         message.channel.send('Pong.');
     } else if (command === "beep") {
         message.channel.send('Boop.');
-    } else if (command === `server`) {
+    } else if (command === `server||Server`) {
         message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
-    } else if (command === `user-info`) {
+    } else if (command === `user-info`|| command ==="User-Info"){
         message.channel.send(`Your username: ${message.author.username}\nYour ID: ${message.author.id}`);
-    } else if (command === "avatar") {
+    } else if (command === "avatar"|| command ==="Avatar") {
         if (!message.mentions.users.size) {
             return message.channel.send(`Your avatar: <${message.author.displayAvatarURL({ dynamic: true })}>`);
         }
@@ -34,12 +34,10 @@ client.on('message', message => {
 
         });
 
-        message.channel.send(avatarList);
-    } else if (command === "Ola") {
-        message.channel.send(`Ola: ${message.author.username}`)
-    } else if (command === "Link"){
+       
+    } else if (command === "Link"|| command ==="link"){
       message.channel.send('``` Organizador de JavaScript```https://beautifier.io/ ```Documentação de PY```https://docs.python.org/pt-br/3 ```Documentação de Discord.JS ``` https://v12.discordjs.guide/creating-your-bot/commands-with-user-input.html#caveats-2\n```Documentação de JavaScript```https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Guide\n```Documentação de Node.JS\n```https://nodejs.org/pt-br/docs/\n```Documentação de PHP\n```https://www.php.net/docs.php\n```Documentação de Ruby\n```https://www.ruby-lang.org/pt/documentation/\n```Documentação de CSS```https://devdocs.io/css/\n``` Documetação de html```https://developer.mozilla.org/pt-BR/docs/Web/HTML')
-    } else if (command ==="Comandos"){
+    } else if (command ==="Comandos"|| command ==="comandos"){
       message.channel.send(`Os comandos disponivel São 
       *Link
       *avatar
