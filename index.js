d7b090b08f4df800a8c249bffee29a8d46de7323
Moderno <83356984+ModernoProgramer@users.noreply.github.com>
2021-11-25 19:59:25 -0300

    Update index.js

--- index.js
+++ index.js
@@ -6,8 +6,47 @@ const {
 const client = new Discord.Client();
 
 client.once('ready', () => {
-    console.log('BOT Online!');
+
+setTimeout(function() {
+    client.user.setActivity('Death Note...', {
+        type: 'WATCHING'
+    })
+}, 60000);
+setTimeout(function() {
+    client.user.setActivity('Naruto...', {
+        type: 'WATCHING'
+    })
+}, 120000);
+setTimeout(function() {
+    client.user.setActivity('Pokémon...', {
+        type: 'WATCHING'
+    })
+}, 180000);
+setTimeout(function() {
+    client.user.setActivity('Dragon Ball Z...', {
+        type: 'WATCHING'
+    })
+}, 240000);
+setTimeout(function() {
+    client.user.setActivity('Spotify...', {
+        type: 'LISTENING'
+    })
+}, 300000);
+setTimeout(function() {
+    client.user.setActivity('lolif hip hop....', {
+        type: 'LISTENING'
+    })
+}, 360000);
+setTimeout(function() {
+    client.user.setActivity('Visual Estudio Code..', {
+        type: 'PLAYING'
+    })
+}, 420000);
+  
+
+    console.log('Bot Online!');
 });
+            
 
 client.on('message', message => {
 
@@ -17,32 +56,13 @@ client.on('message', message => {
     const command = args.shift();
 
     if (command === "ping"|| command ==="Ping") {
-        message.channel.send('Pong.');
-    } else if (command === "beep") {
-        message.channel.send('Boop.');
-    } else if (command === `server||Server`) {
-        message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
-    } else if (command === `user-info`|| command ==="User-Info"){
-        message.channel.send(`Your username: ${message.author.username}\nYour ID: ${message.author.id}`);
-    } else if (command === "avatar"|| command ==="Avatar") {
-        if (!message.mentions.users.size) {
-            return message.channel.send(`Your avatar: <${message.author.displayAvatarURL({ dynamic: true })}>`);
-        }
-
-        const avatarList = message.mentions.users.map(user => {
-            return `${user.username}'s avatar: <${user.displayAvatarURL({ dynamic: true })}>`;
-
-        });
-
        
     } else if (command === "Link"|| command ==="link"){
-      message.channel.send('``` Organizador de JavaScript```https://beautifier.io/ ```Documentação de PY```https://docs.python.org/pt-br/3 ```Documentação de Discord.JS ``` https://v12.discordjs.guide/creating-your-bot/commands-with-user-input.html#caveats-2\n```Documentação de JavaScript```https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Guide\n```Documentação de Node.JS\n```https://nodejs.org/pt-br/docs/\n```Documentação de PHP\n```https://www.php.net/docs.php\n```Documentação de Ruby\n```https://www.ruby-lang.org/pt/documentation/\n```Documentação de CSS```https://devdocs.io/css/\n``` Documetação de html```https://developer.mozilla.org/pt-BR/docs/Web/HTML')
-    } else if (command ==="Comandos"|| command ==="comandos"){
-      message.channel.send(`Os comandos disponivel São 
-      *Link
-      *avatar
-      *user-info
-      *server`)
+      message.channel.send('``` Organizador de JavaScript```https://beautifier.io/ ```Documentação de PY```https://docs.python.org/pt-br/3 ```Documentação de Discord.JS ``` https://v12.discordjs.guide/creating-your-bot/commands-with-user-input.html#caveats-2\n```Documentação de JavaScript```https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Guide\n```Documentação de Node.JS\n```https://nodejs.org/pt-br/docs/\n```Documentação de PHP\n```https://www.php.net/docs.php\n```Documentação de Ruby\n```https://www.ruby-lang.org/pt/documentation/\n```Documentação de CSS```https://devdocs.io/css/\n``` Documetação de html```https://developer.mozilla.org/pt-BR/docs/Web/HTML\n```Site que estou criando o bot```https://replit.com/\n```Site de Hospedagem```https://uptimerobot.com/')
+    } else if (command ==="Comandos"|| command ==="comandos"||command ==="Help"||command==="help"){
+      message.channel.send(`Os comandos disponivel São:
+*Link`)
+
     }
 
 });
